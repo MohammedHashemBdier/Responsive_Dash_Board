63e55f751432294b067e8b704af0770c979e98d3
Mohammed Hashem Bdier <mhb20033@gmail.com>
2024-04-22 22:45:25 +0300

    Create active drawer item

--- lib/widgets/active_and_inactive_item.dart
+++ lib/widgets/active_and_inactive_item.dart
@@ -1,23 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:responsive_dash_board/models/drawer_item_model.dart';
 import 'package:responsive_dash_board/utils/app_styles.dart';
-
-class DrawerItem extends StatelessWidget {
-  const DrawerItem(
-      {super.key, required this.draweritemModel, required this.isActive});
-  final DrawerItemModel draweritemModel;
-  final bool isActive;
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: Colors.white,
-      child: isActive
-          ? ActiveDrawerItem(draweritemModel: draweritemModel)
-          : InActiveDrawerItem(draweritemModel: draweritemModel),
-    );
-  }
-}
 
 class InActiveDrawerItem extends StatelessWidget {
   const InActiveDrawerItem({
