8f98420b27d57d053549c6feee3345a776cdfa31
Mohammed Hashem Bdier <mhb20033@gmail.com>
2024-05-09 15:01:46 +0300

    My cards and Transaction history section

--- lib/widgets/dash_board_desktop_layout.dart
+++ lib/widgets/dash_board_desktop_layout.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/all_expensess_and_quick_invoice_section.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
-import 'package:responsive_dash_board/widgets/transction_history.dart';
+import 'package:responsive_dash_board/widgets/my_cards_and_transction_history_section.dart';
 
 class DashBoardDesktopLayout extends StatelessWidget {
   const DashBoardDesktopLayout({super.key});
@@ -14,12 +14,9 @@ class DashBoardDesktopLayout extends StatelessWidget {
         children: [
           Expanded(child: CustomDrawer()),
           SizedBox(width: 32),
-          Expanded(
-            flex: 2,
-            child: AllExpensessAndQuickInvoiceSection(),
-          ),
+          Expanded(flex: 2, child: AllExpensessAndQuickInvoiceSection()),
           SizedBox(width: 24),
-          Expanded(child: TrasnctionHistory()),
+          Expanded(child: MyCardsAndTransctionHistorySection()),
         ],
       ),
     );
---
+++ lib/widgets/my_cards_and_transction_history_section.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/custom_background_container.dart';
+import 'package:responsive_dash_board/widgets/my_cards_section.dart';
+import 'package:responsive_dash_board/widgets/transction_history.dart';
+
+class MyCardsAndTransctionHistorySection extends StatelessWidget {
+  const MyCardsAndTransctionHistorySection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        SizedBox(height: 40),
+        CustomBackgroundContainer(
+          child: Column(
+            children: [
+              MyCardsSection(),
+              Divider(height: 40, color: Color(0xffF1F1F1)),
+              TrasnctionHistory(),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
