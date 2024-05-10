8fd09f0659ceaac04119c08802c3bb42bd27c573
Mohammed Hashem Bdier <mhb20033@gmail.com>
2024-05-10 17:18:52 +0300

    Responsive tablet layout done

--- lib/widgets/active_and_inactive_item.dart
+++ lib/widgets/active_and_inactive_item.dart
@@ -17,9 +17,13 @@ class InActiveDrawerItem extends StatelessWidget {
       color: Colors.white,
       child: ListTile(
         leading: SvgPicture.asset(draweritemModel.image),
-        title: Text(
-          draweritemModel.title,
-          style: AppStyles.styleMedium16(context),
+        title: FittedBox(
+          fit: BoxFit.scaleDown,
+          alignment: Alignment.centerLeft,
+          child: Text(
+            draweritemModel.title,
+            style: AppStyles.styleMedium16(context),
+          ),
         ),
       ),
     );
@@ -40,9 +44,13 @@ class ActiveDrawerItem extends StatelessWidget {
       color: Colors.white,
       child: ListTile(
         leading: SvgPicture.asset(draweritemModel.image),
-        title: Text(
-          draweritemModel.title,
-          style: AppStyles.styleBold16(context),
+        title: FittedBox(
+          fit: BoxFit.scaleDown,
+          alignment: Alignment.centerLeft,
+          child: Text(
+            draweritemModel.title,
+            style: AppStyles.styleBold16(context),
+          ),
         ),
         trailing: Container(
           width: 4,
--- lib/widgets/user_info_list_tile.dart
+++ lib/widgets/user_info_list_tile.dart
@@ -18,13 +18,21 @@ class UserInfoListTile extends StatelessWidget {
         child: Center(
           child: ListTile(
             leading: SvgPicture.asset(userInfoModel.image),
-            title: Text(
-              userInfoModel.title,
-              style: AppStyles.styleSemiBold16(context),
+            title: FittedBox(
+              fit: BoxFit.scaleDown,
+              alignment: Alignment.centerLeft,
+              child: Text(
+                userInfoModel.title,
+                style: AppStyles.styleSemiBold16(context),
+              ),
             ),
-            subtitle: Text(
-              userInfoModel.subTitle,
-              style: AppStyles.styleRegular12(context),
+            subtitle: FittedBox(
+              fit: BoxFit.scaleDown,
+              alignment: Alignment.centerLeft,
+              child: Text(
+                userInfoModel.subTitle,
+                style: AppStyles.styleRegular12(context),
+              ),
             ),
           ),
         ),
